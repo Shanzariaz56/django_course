b334667290da3df83691ea52ed8e42e4cab5289d
shanzariaz56 <shanzariaz5656@gmail.com>
2023-10-16 23:16:16 -0700

    job_project

--- project/project/settings.py
+++ project/project/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = "project.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+   "default": {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "cars",
+        'USER' : 'postgres',
+        "PASSWORD" : '123',
+        'POST' : 'localhost',
+        'PORT' : 5432,
     }
 }
 
