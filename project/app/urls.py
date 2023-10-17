d9c9bba4091084eabf4dec9e6cd44c030068f196
shanzariaz56 <shanzariaz5656@gmail.com>
2023-10-16 23:02:32 -0700

    job_project

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path("get/",getAllJobs),
+    path("jobs",getAllJobs),
+    path("jobs/<str:pk>",getJobById),
+    path("jobs/new",newJob),
+    path("jobs/update/<str:pk>",updateJob),
+    path("jobs/delete/<str:pk>",deleteJob),
+    path("stats/<str:topic>", getTopicStats),
 ]
